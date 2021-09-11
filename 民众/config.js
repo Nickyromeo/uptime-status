81d79fa310e08630ee1012944e1be387b8e8d721
Nickyromeo <56181327+Nickyromeo@users.noreply.github.com>
2021-09-11 22:12:29 +0800

    Update and rename public/config.js to 民众/config.js

--- 民众/config.js
+++ 民众/config.js
@@ -11,15 +11,12 @@ window.Config = {
   // 只需填写域名部分，默认为官网域名
   // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
   // 详见 https://github.com/yb/uptime-status/ 说明
-  ApiDomain: 'api.uptimerobot.com',
+  ApiDomain: 'status.coolcloud.ga',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1422564-40a1d2bd950aa2deaf31e9a9',
   ],
 
   // 是否显示监测站点的链接
